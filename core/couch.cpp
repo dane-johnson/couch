e243a666f1009e5497cf2d292a8fdcf567d69869
Dane Johnson <dane_johnson@ymail.com>
2021-01-17 12:41:14 -0600

    Add some window hints

--- core/couch.cpp
+++ core/couch.cpp
@@ -48,6 +48,8 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
+  glfwWindowHintString(GLFW_X11_CLASS_NAME, "Couch");
 
   window = glfwCreateWindow(width, height, "Couch", NULL, NULL);
 
