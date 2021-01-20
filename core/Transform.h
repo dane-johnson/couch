d460d1db5025e041ceb8bd20c00b68f5d7e11cbd
Dane Johnson <dane_johnson@ymail.com>
2021-01-20 17:18:39 -0600

    Added scaling

--- core/Transform.h
+++ core/Transform.h
@@ -8,8 +8,10 @@
 struct Transform {
   Transform();
   Transform(Vector3 position, Vector3 rotation);
+  Transform(Vector3 position, Vector3 rotation, Vector3 scale);
   Vector3 position;
   Vector3 rotation;
+  Vector3 scale;
   void Translate(cfloat x, cfloat y, cfloat z);
   Vector3 Forward();
 };
