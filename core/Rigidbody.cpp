1bf1486e0a36a198bb294a2959b9bb917cd52af2
Dane Johnson <dane_johnson@ymail.com>
2021-01-27 00:01:17 -0600

    Refactor and document Rigidbody

--- core/Rigidbody.cpp
+++ core/Rigidbody.cpp
@@ -1,3 +1,24 @@
+/*
+  Dane Johnson <dane@danejohnson.org>
+ 
+  LICENSE
+ 
+  Couch  Copyright (C) 2021 Dane Johnson
+
+  This program comes with ABSOLUTELY NO WARRANTY; without event the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for details at
+  https://www.gnu.org/licenses/gpl-3.0.html
+  
+  This is free software, and you are welcome to redistribute it
+  under the terms of the GNU General Public License as published
+  by the Free Software Foundation; either version 3 of the License,
+  or (at your option) any later version.
+ 
+  DESCRIPTION
+ 
+  Defines physics bodies and their interfaces with the physics engine
+*/
 #include "Rigidbody.h"
 #include "World.h"
 
@@ -31,11 +52,31 @@ Rigidbody *Rigidbody::Instance() {
   return rigidbody;
 }
 
+void Rigidbody::Translate(Vector3 offset) {
+  Spatial::Translate(offset);
+  if (not IsPrefab()) {
+    btBody->translate(btVector3(offset.x,
+				offset.y,
+				offset.z));
+  }
+}
+
 void Rigidbody::SetCollisionShape(CollisionShape collisionShape) {
   if (this->collisionShape) {
     delete this->collisionShape;
   }
   this->collisionShape = collisionShape.shape;
+}
+
+cfloat Rigidbody::GetMass() {
+  return mass;
+}
+
+void Rigidbody::SetMass(cfloat mass) {
+  this->mass = mass;
+  if (not IsPrefab()) {
+    btBody->setMassProps(mass, btVector3(0, 0, 0));
+  }
 }
 
 void Rigidbody::ApplyImpulse(Vector3 impulse) {
@@ -44,6 +85,11 @@ void Rigidbody::ApplyImpulse(Vector3 impulse) {
 
 void Rigidbody::ApplyForce(Vector3 force) {
   btBody->applyCentralForce(btVector3(force.x, force.y, force.z));
+}
+
+
+bool Rigidbody::GetCharacter() {
+  return character;
 }
 
 void Rigidbody::SetCharacter(bool character) {
