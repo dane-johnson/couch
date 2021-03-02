335f596393472e3d43539d5a6a93a21f134d45a4
Dane Johnson <dane_johnson@ymail.com>
2021-03-02 10:39:38 -0600

    Start working on more lights

--- core/Light.h
+++ core/Light.h
@@ -1,3 +1,26 @@
+/**
+   @file
+   @author Dane Johnson <dane@danejohnson.org>
+ 
+   @section LICENSE
+ 
+   Couch  Copyright (C) 2021 Dane Johnson
+
+   This program comes with ABSOLUTELY NO WARRANTY; without event the
+   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+   See the GNU General Public License for details at
+   https://www.gnu.org/licenses/gpl-3.0.html
+  
+   This is free software, and you are welcome to redistribute it
+   under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3 of the License,
+   or (at your option) any later version.
+ 
+   @section DESCRIPTION
+   
+   Lights provide lighting to a scene. They can be colored and support
+   ambient, diffuse, and specular properties.
+*/
 #ifndef LIGHT_H
 #define LIGHT_H
 
