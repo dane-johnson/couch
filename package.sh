664707262dab9de89549f2b627efc99729248944
Dane Johnson <dane_johnson@ymail.com>
2021-03-31 13:45:17 -0500

    Add packing script

--- package.sh
+++ package.sh
@@ -5,9 +5,11 @@ cd `dirname $0`
 
 mkdir -p dist
 
+## Linux build
+
 mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 rm -f couch-linux.tgz
 tar cvzf couch-linux.tgz \
@@ -15,4 +17,23 @@ tar cvzf couch-linux.tgz \
     CORRESPONDINGSOURCE.txt \
     LICENSE
 mv couch-linux.tgz ../dist
+cd ..
 
+## Windows build
+mkdir -p build-win
+cd build-win
+x86_64-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+rm -rf couch-win.zip
+cp ../binaries/*.dll .
+zip couch-win.zip \
+    couch.exe \
+    CORRESPONDINGSOURCE.txt \
+    LICENSE \
+    *.dll
+mv couch-win.zip ../dist
+cd ..
+
+## Source packages
+git archive -o dist/couch-source.tgz master
+git archive -o dist/couch-source.zip master
